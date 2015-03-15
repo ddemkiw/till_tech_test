d6c145cbc7f7b0544def60a61bb942e34a2c7842
Danielle Demkiw <ddemkiw@gmail.com>
2015-03-15 00:14:24 +0000

    fixing before tax tests

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -2,19 +2,26 @@ class Calculator
 
   attr_reader :order, :total, :total_before_tax
 
+
   def initialize(order)
     @order = order
     @prices_per_item = []
-    @total_before_tax = self.total_before_tax
+    @total_before_tax = self.total_without_tax
+    @tax_rate = 0.09
+    @tax = self.tax
   end 
 
   def total
-    @total_before_tax
+    @total_before_tax + @tax
   end
 
-  def total_before_tax 
+  def total_without_tax 
     @order.each{|key, value| @prices_per_item << (key.price * value)}
     @prices_per_item.inject(:+)
+  end 
+
+  def tax
+    @total_before_tax * @tax_rate
   end 
 
 
--- spec/calculating_spec.rb
+++ spec/calculating_spec.rb
@@ -11,7 +11,7 @@ feature 'calculating' do
       item = Item.new({:name=>menu.find("Tiramisu")[0], :price=>menu.find("Tiramisu")[1]})
       order.add(item, 3)
       calculator = Calculator.new(order.line_items)
-      receipt = Receipt.new(order, calculator.total)
+      receipt = Receipt.new(order, calculator.total_before_tax)
 
       expect(receipt.total).to eq(34.2)
 
@@ -25,9 +25,8 @@ feature 'calculating' do
       item2 = Item.new({:name=>menu.find("Americano")[0], :price=>menu.find("Americano")[1]})
       order.add(item1, 3)
       order.add(item2, 3)
-      p order.line_items
       calculator = Calculator.new(order.line_items)
-      receipt = Receipt.new(order, calculator.total)
+      receipt = Receipt.new(order, calculator.total_before_tax)
 
       expect(receipt.total).to eq(45.45)
 
