0d6d02c949a19e837564b418a6ad51e6f745a7f1
Danielle Demkiw <ddemkiw@gmail.com>
2015-03-16 12:55:03 +0000

    adding tests for calculator

--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -11,6 +11,10 @@ describe 'Calculator' do
     expect(calulator.total).to eq(37.15)
   end 
 
+   it 'adds up the bill without tax' do
+    expect(calulator.total_before_tax).to eq(34.2)
+  end 
+
   it 'calculates the tax for the bill' do
     expect(calulator.tax).to eq(2.95)
   end
