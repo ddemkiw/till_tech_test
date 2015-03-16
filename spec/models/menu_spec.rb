dc1f46cbe51aebc7f63466257c0da92da705c51f
Danielle Demkiw <ddemkiw@gmail.com>
2015-03-16 13:57:58 +0000

    unit test for menu

---
+++ spec/models/menu_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'Menu' do 
+
+  let(:menu) { Menu.new }
+
+  it 'contains a list of items' do
+    expect(menu.items).to include('Americano')
+  end 
+
+   it 'can find an item based on item name' do
+    expect(menu.find('Americano')).to include('Americano')
+  end 
+
+
+end
